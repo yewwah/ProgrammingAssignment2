2412ecd796320d6868d3685e91e2f774bb967510
yewwah <yewwah10@gmail.com>
2014-09-21 21:42:38 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,4 @@
-# See README.md for instructions on running the code and output from it
-# The assignment states that running the code is not part of the grading 
-# but I have the instructions anyway.
+
 
 # makeCacheMatrix is a function that returns a list of functions
 # Its puspose is to store a martix and a cached value of the inverse of the 
@@ -10,10 +8,7 @@
 # * cacheInverse   get the cahced value (inverse of the matrix)
 # * getInverse     get the cahced value (inverse of the matrix)
 #
-# Notes:
-# not sure how the "x = numeric()" part works in the argument list of the 
-# function, but it seems to be creating a variable "x" that is not reachable 
-# from the global environment, but is available in the environment of the 
+ 
 # makeCacheMatrix function
 makeCacheMatrix <- function(x = numeric()) {
         
@@ -53,13 +48,12 @@ makeCacheMatrix <- function(x = numeric()) {
 cacheSolve <- function(y, ...) {
         # get the cached value
         inverse <- y$getInverse()
-        # if a cached value exists return it
+        
         if(!is.null(inverse)) {
-                message("getting cached data")
+                message("obtaining cached data")
                 return(inverse)
         }
-        # otherwise get the matrix, caclulate the inverse and store it in
-        # the cache
+
         data <- y$getMatrix()
         inverse <- solve(data)
         y$cacheInverse(inverse)
